6cdc6889e34815119dc96d26e438f31eb121987a
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2023-12-18 16:58:11 +0330

    chore: main app, router inclusion and middleware inclusion.

--- Account/main.py
+++ Account/main.py
@@ -1,3 +1,16 @@
+# In the name of GOD
+
+from fastapi import FastAPI, Depends, Request
+from fastapi.responses import JSONResponse
+from contextlib import asynccontextmanager
+
+
+from api.v1 import internal_users_api, external_users_api
+from db.db import collections, databases, sess_db
+from config.middlewares import InternalSecurityMiddleware
+
+
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     account_db = await sess_db()
@@ -5,3 +18,18 @@ async def lifespan(app: FastAPI):
     yield
     collections.clear()
     account_db.client.close()
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(internal_users_api.router)
+app.include_router(external_users_api.router)
+app.add_middleware(InternalSecurityMiddleware)
+
+
+@app.get("/")
+def root():
+    return JSONResponse({"message": "Hello..."}, status_code=200)
+
+if __name__ == '__main__':
+    import uvicorn
+    # uvicorn.run("main:app", host="localhost", port=8003, log_level="debug", reload=True)
+    uvicorn.run(app, host="localhost", port=8003, log_level="debug")
