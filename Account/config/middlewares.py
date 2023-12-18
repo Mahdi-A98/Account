1ea4f6423c63d108b446121fe74ea198aaa0fbb4
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2023-12-18 21:20:52 +0330

    chore: old tries to modify request and response.

--- Account/config/middlewares.py
+++ Account/config/middlewares.py
@@ -1,3 +1,5 @@
+# In the name of GOD
+
 from starlette.middleware.base import BaseHTTPMiddleware
 from starlette.types import Scope, Receive, Message
 from fastapi import Request, Response
@@ -34,3 +36,45 @@ class InternalSecurityMiddleware(BaseHTTPMiddleware):
         response = await self.encrypte_internal_response(request, response)
         return response
 
+
+    # async def __call__(self, scope, receive, send):
+    #     if scope["type"] != "http":
+    #         what = await self.app(scope, receive, send)
+    #         return
+    #     body_size = 0
+
+    #     async def receive_logging_request_body_size():
+    #         nonlocal body_size
+    #         message = await receive()
+    #         assert message["type"] == "http.request"
+    #         body_size += len(message.get("body", b""))
+    #         if not message.get("more_body", False):
+    #             print(f"Size of request body was: {body_size} bytes")
+    #         print(f"\n{'=='*25}\nmessage: {message}\t message type: {type(message)}\n")
+    #         return message
+
+    #     async def send_with_reader(message):
+    #         print(f"\n {'=='*25}\ngetattr(self, \'current_request\', None)  : {getattr(self, 'current_request', None)}\n")
+    #         if message["type"] == "http.response.body":
+    #             print("iiiiiiiiiiiiiiiiiiiiiiiiiiiiiii")
+    #             # Read the response body as bytes
+    #             message['body'] = await self.encrypte_internal_response(self.current_request, message['body'].decode())
+    #             body = message.get("body", b"")
+    #             # Decode the bytes to a string
+    #             body_str = body.decode()
+    #             # Print the response body to the console
+    #             print(f"Response body: {body_str}")
+    #         print(f"\n {'=='*25}\nmessage in sendss: {message}\t message type: {type(message)}\n")
+    #         await send(message)
+
+    #     await super().__call__(scope, receive_logging_request_body_size, send_with_reader)
+
+        # async def encrypte_internal_response(self, request: Request, message: bytes):
+    #     if internal_service:= SERVICE_CLASSES.get(request.headers.get("internal-service")):
+    #         # # Consuming FastAPI response and grabbing body here
+    #         # resp_body = [section async for section in response.__dict__['body_iterator']]
+    #         # # Repairing FastAPI response
+    #         # response.__setattr__('body_iterator', aiwrap(resp_body)
+    #         message = internal_service.Encrypt_tools.encrypt_data(message)
+    #     return message.encode()
+        
