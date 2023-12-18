08789aaeac06c4e60ee74b0659432a3302b72c87
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2023-12-18 21:08:26 +0330

    chore: fastapi middleware to catch, decode request and encode response of interservices requests.

---
+++ Account/config/middlewares.py
@@ -0,0 +1,8 @@
+from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.types import Scope, Receive, Message
+from fastapi import Request, Response
+
+from services import SERVICE_CLASSES
+from utils.async_iterator import async_iterator_wrapper as aiwrap
+
+class InternalSecurityMiddleware(BaseHTTPMiddleware):
