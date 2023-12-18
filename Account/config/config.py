8aaca42159df8b4b603c50858e838788b67d67f3
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2023-12-18 16:42:10 +0330

    chore: logging configuration

--- Account/config/config.py
+++ Account/config/config.py
@@ -1,3 +1,6 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+import logging.config
+
 class Settings(BaseSettings):
     MONGODB_HOST : str
     MONGODB_PORT : str
@@ -21,3 +24,33 @@ class Settings(BaseSettings):
     POD_ACC_SHARED_KEY : str
     
     model_config = SettingsConfigDict(env_file=".env", extra="allow")
+
+
+LOGGING_CONFIG = {
+    "version": 1,
+    "handlers": {
+        "default": {
+            "class": "logging.StreamHandler",
+            "formatter": "http",
+            "stream": "ext://sys.stderr"
+        }
+    },
+    "formatters": {
+        "http": {
+            "format": "%(levelname)s [%(asctime)s] %(name)s - %(message)s",
+            "datefmt": "%Y-%m-%d %H:%M:%S",
+        }
+    },
+    'loggers': {
+        'httpx': {
+            'handlers': ['default'],
+            'level': 'DEBUG',
+        },
+        'httpcore': {
+            'handlers': ['default'],
+            'level': 'DEBUG',
+        },
+    }
+}
+
+logging.config.dictConfig(LOGGING_CONFIG)
