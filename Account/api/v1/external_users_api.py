69dba96037a5a6bf9fc2ed3e513fc94a1c0b42c4
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2023-12-18 22:10:45 +0330

    chore: user profile endpoint

--- Account/api/v1/external_users_api.py
+++ Account/api/v1/external_users_api.py
@@ -12,3 +12,18 @@ async def update_profile(user_data:LoginDep, update_data: UserUpdate = Body(...)
     result = await account_db.update_one(filter={"username": user_data.get("data").get("username")}, update={"$set": update_data.dict(exclude_none=True)})
 
     return JSONResponse({"message": str(result)}, status_code=status.HTTP_200_OK)
+
+@router.get(
+
+    "/user_profile",
+    response_description="User profile for internal services access",
+)
+async def user_profile(user_data:LoginDep): # depend on login
+
+    account_db = collections['users_collection']
+    user_profile_data = await account_db.find_one(filter={"$or":[{"username":{"$eq": user_data["data"]["username"], "$ne": None}},
+                                                        {"email": {"$eq":user_data["data"].get("email", "no email"), "$ne": None}}]})
+    if not user_profile_data:
+        return JSONResponse({"message": "User Not found"}, status_code=status.HTTP_404_NOT_FOUND)
+    user_profile_data.pop("_id")
+    return JSONResponse({"user_profile": user_profile_data}, status_code=status.HTTP_200_OK)
